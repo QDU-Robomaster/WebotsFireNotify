d953e93bdf51a62ac620594925bfce7354c07df6
Jiu-xiao <2592509183@qq.com>
2025-10-11 00:42:09 +0800

    Init

---
+++ README.md
@@ -0,0 +1,15 @@
+# WebotsFireNotify
+
+No description provided
+
+## Required Hardware
+None
+
+## Constructor Arguments
+None
+
+## Template Arguments
+None
+
+## Depends
+None
---
+++ WebotsFireNotify.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+// clang-format off
+/* === MODULE MANIFEST V2 ===
+module_description: No description provided
+constructor_args: []
+template_args: []
+required_hardware: []
+depends: []
+=== END MANIFEST === */
+// clang-format on
+
+#include <cstdint>
+#include <webots/LED.hpp>
+#include <webots/Robot.hpp>
+
+#include "app_framework.hpp"
+#include "libxr.hpp"
+
+extern webots::Robot *_libxr_webots_robot_handle;
+
+class WebotsFireNotify : public LibXR::Application
+{
+ public:
+  WebotsFireNotify(LibXR::HardwareContainer &, LibXR::ApplicationManager &app)
+  {
+    led_ = _libxr_webots_robot_handle->getLED("fire_led");
+
+    auto cb = LibXR::Topic::Callback::Create(
+        [](bool, WebotsFireNotify *self, LibXR::RawData &data)
+        {
+          uint8_t *msg = static_cast<uint8_t *>(data.addr_);
+          self->led_->set(*msg > 0 ? 255 : 0);
+        },
+        this);
+
+    fire_notify_topic_.RegisterCallback(cb);
+
+    app.Register(*this);
+  }
+
+  void OnMonitor() override {}
+
+ private:
+  webots::LED *led_;
+
+  LibXR::Topic fire_notify_topic_ = LibXR::Topic("fire_notify", sizeof(uint8_t));
+};
